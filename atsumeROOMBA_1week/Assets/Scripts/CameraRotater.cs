054b7cbf19f94e98bd415541a51cdb08ccc5b26f
Kouki NAGATA <kouki0207grape@icloud.com>
2019-07-05 04:56:42 +0900

    ルンバの移動と回転
    カメラが正面を向く方にルンバに移動してもらうことにしました

---
+++ atsumeROOMBA_1week/Assets/Scripts/CameraRotater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotater : MonoBehaviour
+{
+    public GameObject player;//カメラを基準に移動させる
+    public float horizontalInput;//横矢印キー
+    private Vector3 ROOMBAPos;//ルンバの座標
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("ROOMBA");
+        ROOMBAPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        // ROOMBAの移動量分、カメラも移動する
+        transform.position += player.transform.position - ROOMBAPos;
+        ROOMBAPos = player.transform.position;
+
+        // ルンバの位置のY軸を中心に、カメラを回転（公転）させる
+        transform.RotateAround(ROOMBAPos, Vector3.up, horizontalInput * Time.deltaTime * 200f);
+    }
+}
---
+++ atsumeROOMBA_1week/Assets/Scripts/ROOMBAMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ROOMBAMover : MonoBehaviour
+{
+    public Rigidbody rb;
+    private bool _pushed;//スペースキーが押されているかの判定
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _pushed = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _pushed = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // カメラの方向から、X-Z平面の単位ベクトルを取得
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 moveForward = cameraForward * 7;
+
+        if (_pushed)
+        {
+            rb.AddForce(moveForward);
+        }
+    }
+}
